158274fd3a058ffa96f75eeb75389b9e1e2506ed
Connor Slade <connor@connorcode.com>
2021-10-13 23:00:59 -0400

    🧱 Cleanup

--- static/index.js
+++ static/index.js
@@ -1,22 +1,25 @@
 // TODO: Gen colors with Item Index
 
-// Colors
-// [ START COLOR END COLOR ]
+// Colors StartColor -> End Color
 const colors = [
   [7, 142, 112],
   [59, 12, 139],
 ];
 
 function updateEvents() {
+  // Get Schedule
   fetch("/schedule")
     .then((resp) => resp.json())
     .then((data) => {
+      // Find how much to change the R, G, and B channel for each dot
+      // to make a gradent between color 0 and color 1
       let changeBy = [
         (colors[1][0] - colors[0][0]) / (data.length - 1),
         (colors[1][1] - colors[0][1]) / (data.length - 1),
         (colors[1][2] - colors[0][2]) / (data.length - 1),
       ];
 
+      // Create the dots
       data.forEach((i, index) => {
         let newDiv = document.createElement("div");
         newDiv.innerHTML = `<div class="event"><div class="dot" style="background: rgb(${toRgb(
@@ -26,15 +29,20 @@ function updateEvents() {
         )});"></div>${i.name}</div>`;
         document.querySelector("#container").appendChild(newDiv);
       });
+
+      // Show the back line
+      document.querySelector('#line').style.display = 'block';
     });
 }
 
 window.addEventListener("load", updateEvents);
 
+// Convert an array of R, G, B to a string
 function toRgb(n) {
   return `${n[0]},${n[1]},${n[2]}`;
 }
 
+// Add a value to the R, G and B channel
 function lightenRgb(rgb, light) {
   return [
     Math.min(rgb[0] + light, 255),
@@ -43,6 +51,7 @@ function lightenRgb(rgb, light) {
   ];
 }
 
+// Apply the ChangeBy values for each itaration
 function applyRgbChange(rgb, change, i) {
   return [
     rgb[0] + change[0] * i,
